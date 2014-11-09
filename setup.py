6c20122923b132b4f759b47ea5e3d92ef6923a65
ahmedshabib <ahmed.bolwar@gmail.com>
2014-11-10 00:18:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2',
     packages=['Parsly'],
     url='http://github.com/ahmedshabib/parsly',
-    license='BSD License',
+    license='MIT License',
     author='Ahmed Shabib',
     author_email='reachshabib@gmail.com',
     description='Scraping Websites Done right'
